b4fa77c04672ec184e7226513f122e27684df74a
niklas <niklas.froese@dfx.swiss>
2022-11-30 13:21:47 +0100

    [DEV-1085] dark-mode fix in tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
-  darkMode: false,
+  darkMode: 'media',
   theme: {
     colors: {
       white: '#ffffff',
