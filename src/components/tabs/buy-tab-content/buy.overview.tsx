128b272dd641dd12674e8a5eb51263c7158ca9cc
Krysh <krysh.developer@gmail.com>
2022-12-09 09:53:21 +0100

    [DEV-1164] adapted to coin list changes

--- src/components/tabs/buy-tab-content/buy.overview.tsx
+++ src/components/tabs/buy-tab-content/buy.overview.tsx
@@ -1,6 +1,8 @@
 import { useAssetContext } from '../../../api/contexts/asset.context';
 import { Asset } from '../../../api/definitions/asset';
-import DfxIcon, { IconVariant } from '../../../stories/DfxIcon';
+import { Blockchain } from '../../../api/definitions/blockchain';
+import StyledCoinList from '../../../stories/StyledCoinList';
+import StyledCoinListItem, { Protocols } from '../../../stories/StyledCoinListItem';
 
 interface BuyTabContentOverviewProps {
   onAssetClicked: (asset: Asset) => void;
@@ -8,22 +10,30 @@ interface BuyTabContentOverviewProps {
 
 export function BuyTabContentOverview({ onAssetClicked }: BuyTabContentOverviewProps): JSX.Element {
   const { buyableAssets } = useAssetContext();
+
+  const headings: Record<Blockchain, string> = {
+    [Blockchain.ETH]: 'Ethereum mainnet · ERC-20 token',
+    [Blockchain.BSC]: 'Binance Smart Chain · BEP-20 token',
+  };
+
+  const protocols: Record<Blockchain, Protocols> = {
+    [Blockchain.ETH]: Protocols.ERC_20,
+    [Blockchain.BSC]: Protocols.BEP_20,
+  };
+
   return (
     <div className="flex flex-col gap-8">
       {Array.from(buyableAssets.entries()).map(([blockchain, assets], blockchainIndex) => (
-        <div key={blockchainIndex} className="flex flex-col gap-4">
-          <p>
-            <strong>{blockchain}</strong>
-          </p>
-          <div className="flex flex-row gap-4">
-            {assets.map((asset, assetIndex) => (
-              <button key={assetIndex} className="flex flex-row gap-1" onClick={() => onAssetClicked(asset)}>
-                <DfxIcon icon={IconVariant.INFO} />
-                <p>{asset.name}</p>
-              </button>
-            ))}
-          </div>
-        </div>
+        <StyledCoinList key={blockchainIndex} heading={headings[blockchain]}>
+          {assets.map((asset, assetIndex) => (
+            <StyledCoinListItem
+              key={assetIndex}
+              asset={asset.name}
+              protocol={protocols[blockchain]}
+              onClick={() => onAssetClicked(asset)}
+            />
+          ))}
+        </StyledCoinList>
       ))}
     </div>
   );
