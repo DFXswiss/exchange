2a420c08bb0c85864f7e732caff4e92639a4017c
Krysh <krysh.developer@gmail.com>
2022-12-12 10:29:34 +0100

    [DEV-1164] fixed issue with undefined detailLabelFunc

--- src/stories/form/StyledModalDropdown.tsx
+++ src/stories/form/StyledModalDropdown.tsx
@@ -6,7 +6,7 @@ import { ControlProps } from './Form';
 interface StyledModalDropdownProps extends ControlProps {
   placeholder: string;
   labelFunc: (item: any) => string;
-  detailLabelFunc: (item: any) => string;
+  detailLabelFunc?: (item: any) => string;
   modal: {
     heading: string;
     items: any;
@@ -62,7 +62,7 @@ const StyledModalDropdown = forwardRef<any, any>(
             <div className="flex flex-col gap-1 py-4">
               <label className="text-dfxBlue-800 text-base font-semibold pl-4">{label}</label>
               <button onClick={() => setShowModal(true)}>
-                {value && <p className="text-dfxGray-600">{detailLabelFunc(value)}</p>}
+                {value && detailLabelFunc && <p className="text-dfxGray-600">{detailLabelFunc(value)}</p>}
                 <p
                   className={`text-base font-normal border border-dfxGray-500 rounded-md p-3 ${
                     value ? 'text-dfxBlue-800' : 'text-dfxGray-600'
