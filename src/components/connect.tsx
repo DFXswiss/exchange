14f9e4c92581fef2ed3b86e25ecf73758edd0303
David May <david.leo.may@gmail.com>
2022-12-12 20:42:44 +0100

    Added a TnC callback

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -2,15 +2,30 @@ import { useWalletContext } from '../contexts/wallet.context';
 import { useSessionContext } from '../contexts/session.context';
 import StyledButton, { StyledButtonColors, StyledButtonWidths } from '../stories/StyledButton';
 import StyledModal, { StyledModalTypes } from '../stories/StyledModal';
+import { useState } from 'react';
+import { useDeferredPromise } from '../hooks/deferred-promise.hook';
 
 export function Connect(): JSX.Element {
   const { isConnected } = useWalletContext();
-  const { needsSignUp, login, signUp } = useSessionContext();
+  const { login } = useSessionContext();
+  const { defer, deferRef } = useDeferredPromise<void>();
+
+  const [showTnc, setShowTnc] = useState(false);
+
+  function confirmTnc(): Promise<void> {
+    setShowTnc(true);
+    return defer().promise;
+  }
+
+  function tncConfirmed() {
+    setShowTnc(false);
+    deferRef?.resolve();
+  }
 
   return (
     <>
       {/* MODALS */}
-      <StyledModal type={StyledModalTypes.ALERT} isVisible={needsSignUp}>
+      <StyledModal type={StyledModalTypes.ALERT} isVisible={showTnc}>
         <div className="flex flex-col">
           <h1>Terms and Conditions.</h1>
           <p>
@@ -32,11 +47,11 @@ export function Connect(): JSX.Element {
           color={StyledButtonColors.RED}
           label="Next"
           caps={false}
-          onClick={() => signUp()}
+          onClick={tncConfirmed}
         />
       </StyledModal>
       <div className={isConnected ? 'hidden' : ''}>
-        <StyledButton label="Connect to Metamask" onClick={login} />
+        <StyledButton label="Connect to Metamask" onClick={() => login(confirmTnc)} />
       </div>
     </>
   );
--- src/contexts/session.context.tsx
+++ src/contexts/session.context.tsx
@@ -1,4 +1,4 @@
-import { createContext, PropsWithChildren, useContext, useState } from 'react';
+import { createContext, PropsWithChildren, useContext } from 'react';
 import { Blockchain } from '../api/definitions/blockchain';
 import { ApiError } from '../api/definitions/error';
 import { useApiSession } from '../api/hooks/api-session.hook';
@@ -9,9 +9,7 @@ export interface SessionInterface {
   address?: string;
   blockchain?: Blockchain;
   isLoggedIn: boolean;
-  needsSignUp: boolean;
-  login: () => Promise<void>;
-  signUp: () => Promise<void>;
+  login: (tncCallback?: () => Promise<void>) => Promise<void>;
 }
 
 const SessionContext = createContext<SessionInterface>(undefined as any);
@@ -23,10 +21,8 @@ export function useSessionContext(): SessionInterface {
 export function SessionContextProvider(props: PropsWithChildren): JSX.Element {
   const { isLoggedIn, getSignMessage, createSession } = useApiSession();
   const { isInstalled, isConnected, address, blockchain, connect, signMessage } = useWalletContext();
-  const [needsSignUp, setNeedsSignUp] = useState(false);
-  const [signature, setSignature] = useState<string>();
 
-  async function login(): Promise<void> {
+  async function login(tncCallback?: () => Promise<void>): Promise<void> {
     let connectedAddress = address;
     if (!isConnected) {
       connectedAddress = await connect();
@@ -34,25 +30,26 @@ export function SessionContextProvider(props: PropsWithChildren): JSX.Element {
     if (!connectedAddress) return; // TODO (Krysh) add real error handling in here
     // if we are already logged in, do not trigger another signature
     if (isLoggedIn) return;
+
     const message = await getSignMessage(connectedAddress);
     const signature = await signMessage(message, connectedAddress);
-    return createSession(connectedAddress, signature, false).catch((error: ApiError) => {
-      if (error.statusCode === 404) {
-        setSignature(signature);
-        setNeedsSignUp(true);
+
+    return loginToApi(connectedAddress, signature, tncCallback);
+  }
+
+  async function loginToApi(address: string, signature: string, waitForTnc?: () => Promise<void>): Promise<void> {
+    createSession(address, signature, false).catch((error: ApiError) => {
+      if (error.statusCode === 404 && waitForTnc) {
+        return waitForTnc().then(() => signUp(address, signature));
       }
     });
   }
 
-  async function signUp(): Promise<void> {
-    if (!address || !signature) return; // TODO (Krysh) add real error handling
-    return createSession(address, signature, true).finally(() => {
-      setSignature(undefined);
-      setNeedsSignUp(false);
-    });
+  async function signUp(address: string, signature: string): Promise<void> {
+    return createSession(address, signature, true);
   }
 
-  const context = { isMetaMaskInstalled: isInstalled, address, blockchain, isLoggedIn, needsSignUp, login, signUp };
+  const context = { isMetaMaskInstalled: isInstalled, address, blockchain, isLoggedIn, login };
 
   return <SessionContext.Provider value={context}>{props.children}</SessionContext.Provider>;
 }
