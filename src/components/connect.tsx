254de92ef8f6fed4abc2da99f20bafcf6196749b
Krysh <krysh.developer@gmail.com>
2022-12-12 19:21:44 +0100

    [DEV-1167] CRC

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -1,14 +1,43 @@
 import { useWalletContext } from '../contexts/wallet.context';
 import { useSessionContext } from '../contexts/session.context';
-import StyledButton from '../stories/StyledButton';
+import StyledButton, { StyledButtonColors, StyledButtonWidths } from '../stories/StyledButton';
+import StyledModal, { StyledModalTypes } from '../stories/StyledModal';
 
 export function Connect(): JSX.Element {
   const { isConnected } = useWalletContext();
-  const { login } = useSessionContext();
+  const { needsSignUp, login, signUp } = useSessionContext();
 
   return (
-    <div className={isConnected ? 'hidden' : ''}>
-      <StyledButton label="Connect to Metamask" onClick={login} />
-    </div>
+    <>
+      {/* MODALS */}
+      <StyledModal type={StyledModalTypes.ALERT} isVisible={needsSignUp}>
+        <div className="flex flex-col">
+          <h1>Terms and Conditions.</h1>
+          <p>
+            Please read our terms and conditions and click on ”Next” to confirm and to continue to the DFX Multichain
+            Service.
+          </p>
+          <a
+            className="underline underline-offset-2 pt-6 pb-4"
+            target="_blank"
+            href={process.env.REACT_APP_TNC_URL}
+            rel="noopener noreferrer"
+          >
+            Terms and conditions DFX Swiss.
+          </a>
+        </div>
+
+        <StyledButton
+          width={StyledButtonWidths.MD}
+          color={StyledButtonColors.RED}
+          label="Next"
+          caps={false}
+          onClick={() => signUp()}
+        />
+      </StyledModal>
+      <div className={isConnected ? 'hidden' : ''}>
+        <StyledButton label="Connect to Metamask" onClick={login} />
+      </div>
+    </>
   );
 }
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,5 @@
-import { useSessionContext } from '../contexts/session.context';
 import DfxLogo from '../stories/DfxLogo';
 import DfxTitleSection from '../stories/DfxTitleSection';
-import StyledButton, { StyledButtonColors, StyledButtonWidths } from '../stories/StyledButton';
-import StyledModal, { StyledModalTypes } from '../stories/StyledModal';
 import StyledTabContainer, { StyledTabProps } from '../stories/StyledTabContainer';
 import { Connect } from './connect';
 import { BuyTab } from './tabs/buy.tab';
@@ -10,7 +7,6 @@ import { UserBox } from './user-box';
 import { WalletBox } from './wallet-box';
 
 export function Main(): JSX.Element {
-  const { needsSignUp, signUp } = useSessionContext();
   function buildComingSoonTab(title: string): StyledTabProps {
     return {
       title,
@@ -22,60 +18,25 @@ export function Main(): JSX.Element {
   }
 
   return (
-    <>
-      {/* MODALS */}
-      <StyledModal
-        type={StyledModalTypes.ALERT}
-        isVisible={needsSignUp}
-        onClose={() => {
-          // intentionally do nothing, as alert are closed via button
-        }}
-      >
-        <div className="flex flex-col">
-          <h1>Terms and Conditions.</h1>
-          <p>
-            Please read our terms and conditions and click on ”Next” to confirm and to continue to the DFX Multichain
-            Service.
-          </p>
-          <a
-            className="underline underline-offset-2 pt-6 pb-4"
-            target="_blank"
-            href={process.env.REACT_APP_TNC_URL}
-            rel="noopener noreferrer"
-          >
-            Terms and conditions DFX Swiss.
-          </a>
+    <div className="text-center p-2 h-screen">
+      <div className="max-w-6xl text-left mx-auto ">
+        <div className="flex justify-between">
+          <DfxLogo />
+          <Connect />
         </div>
-
-        <StyledButton
-          width={StyledButtonWidths.MD}
-          color={StyledButtonColors.RED}
-          label="Next"
-          caps={false}
-          onClick={() => signUp()}
+        <div className="md:flex justify-between mt-6">
+          <div className="basis-3/5 max-w-[50%] px-6 mx-auto md:mx-0">
+            <DfxTitleSection heading="DFX Multichain" subheading="Buy • Sell • Convert • Stake" />
+          </div>
+          <aside className="basis-2/5 shrink-0 md:min-w-[470px] lg:min-w-[512px] mx-auto md:mx-0">
+            <WalletBox />
+            <UserBox />
+          </aside>
+        </div>
+        <StyledTabContainer
+          tabs={[BuyTab, buildComingSoonTab('Sell'), buildComingSoonTab('Convert'), buildComingSoonTab('Staking')]}
         />
-      </StyledModal>
-      {/* CONTENT */}
-      <div className="text-center p-2 h-screen">
-        <div className="max-w-6xl text-left mx-auto ">
-          <div className="flex justify-between">
-            <DfxLogo />
-            <Connect />
-          </div>
-          <div className="md:flex justify-between mt-6">
-            <div className="basis-3/5 max-w-[50%] px-6 mx-auto md:mx-0">
-              <DfxTitleSection heading="DFX Multichain" subheading="Buy • Sell • Convert • Stake" />
-            </div>
-            <aside className="basis-2/5 shrink-0 md:min-w-[470px] lg:min-w-[512px] mx-auto md:mx-0">
-              <WalletBox />
-              <UserBox />
-            </aside>
-          </div>
-          <StyledTabContainer
-            tabs={[BuyTab, buildComingSoonTab('Sell'), buildComingSoonTab('Convert'), buildComingSoonTab('Staking')]}
-          />
-        </div>
       </div>
-    </>
+    </div>
   );
 }
