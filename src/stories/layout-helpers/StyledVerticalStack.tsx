bca48b5fd4b422cf02f92bf0f85abc976171061d
niklas <niklas.froese@dfx.swiss>
2023-01-23 10:25:30 +0100

    [DEV-1085] Implemented PR-Feedback

--- src/stories/layout-helpers/StyledVerticalStack.tsx
+++ src/stories/layout-helpers/StyledVerticalStack.tsx
@@ -10,10 +10,10 @@ export interface StyledVerticalStackProps extends PropsWithChildren {
 
 export default function StyledVerticalStack({ children, gap = 0, marginY, marginX, center }: StyledVerticalStackProps) {
   const spacing = convertToRem(gap);
-  const mY = marginY !== undefined ? convertToRem(marginY) : '0';
-  const mX = marginX !== undefined ? convertToRem(marginX) : '0';
+  const mY = marginY != undefined ? convertToRem(marginY) : '0';
+  const mX = marginX != undefined ? convertToRem(marginX) : '0';
   let classNames = 'flex flex-col ';
-  classNames += center && ' items-center';
+  center && (classNames += 'items-center');
 
   return (
     <div style={{ gap: spacing, margin: mY + ' ' + mX }} className={classNames}>
